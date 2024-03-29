8ef41caedb8aa16244b0dea85a90bde63f15d30b
Dimitri Teixeira <dimitriteixeira@gmail.com>
2024-03-29 16:54:45 -0300

    Ajustado Horário Home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,18 +1,33 @@
+'use client'
+import { useState, useEffect } from 'react';
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  const now = new Date();
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-  const time = now.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('pt-BR', { dateStyle: 'full' })).format(now);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // Atualiza a cada segundo
+
+    // Limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(intervalId);
+  }, []); // Executa apenas uma vez ao montar o componente
+
+  // Ajusta o tempo para -3 horas
+  const adjustedTime = new Date(currentTime.getTime() - 3 * 60 * 60 * 1000);
+  const hours = adjustedTime.getHours().toString().padStart(2, '0');
+  const minutes = adjustedTime.getMinutes().toString().padStart(2, '0');
+  const seconds = adjustedTime.getSeconds().toString().padStart(2, '0');
+  const time = `${hours}:${minutes}`;//add :${seconds} se quiser
+
+  // Formatação da data
+  const date = (new Intl.DateTimeFormat('pt-BR', { dateStyle: 'full' })).format(adjustedTime);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-end max-md:px-5 max-md:py-8 lg:p-11">
-          {/* <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Você está no DimitriConnect
-          </h2> */}
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
